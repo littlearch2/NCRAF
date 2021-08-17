ac95388627d33de549ad75d2dc42070b3783a21d
littlearch2 <89084064+littlearch2@users.noreply.github.com>
2021-08-17 12:38:09 -0400

    Add files via upload

---
+++ NCRAF/Addons/ncraf_factions/CfgGroupsWest.hpp
@@ -0,0 +1,572 @@
+class WEST {
+
+	class ncraf_groups_ncr {
+		name = "[NCRAF] New California Republic Military";
+		
+		class Infantry {
+		name = "Infantry";
+		
+	class ncraf_infantry_conscript_squad {
+		name = "Conscript Squad";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf";
+		rarityGroup = 0.9;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "SERGEANT";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_sl";
+		};
+		class Unit1 {
+		position[] = {3,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_conscript";
+		};
+		class Unit2 {
+		position[] = {4,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_conscript";
+		};
+		class Unit3 {
+		position[] = {5,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_conscript";
+		};
+		class Unit4 {
+		position[] = {6,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_conscript";
+		};
+		class Unit5 {
+		position[] = {3,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_conscript";
+		};
+		class Unit6 {
+		position[] = {4,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_conscript";
+		};
+		class Unit7 {
+		position[] = {5,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_conscript";
+		};
+		class Unit8 {
+		position[] = {6,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_conscript";
+		};
+		class Unit9 {
+		position[] = {3,-5,0};
+		rank = "CORPORAL";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+	};
+	
+	class ncraf_infantry_rifle_squad {
+		name = "Rifle Squad";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+		raritygroup = 0.8;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "SERGEANT";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_sl";
+		};
+		class Unit1 {
+		position[] = {3,-1,0};
+		rank = "CORPORAL";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_tl";
+		};
+		class Unit2 {
+		position[] = {4,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_autorifle";
+		};
+		class Unit3 {
+		position[] = {5,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_grenadier";
+		};
+		class Unit5 {
+		position[] = {6,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+		class Unit6 {
+		position[] = {3,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+		class Unit7 {
+		position[] = {4,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+		class Unit8 {
+		position[] = {5,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+		class Unit9 {
+		position[] = {6,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+	};
+
+	class ncraf_infantry_rifle_team  {
+		name = "Rifle Team";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+		raritygroup = 0.8;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "CORPORAL";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_tl";
+		};
+		class Unit1 {
+		position[] = {3,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+		class Unit2 {
+		position[] = {4,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+		class Unit3 {
+		position[] = {5,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+	};
+	
+	class ncraf_infantry_weapon_squad {
+		name = "Weapon Squad";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+		raritygroup = 0.6;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "SERGEANT";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_sl";
+		};
+		class Unit1 {
+		position[] = {4,-1,0};
+		rank = "CORPORAL";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_tl";
+		};
+		class Unit2 {
+		position[] = {5,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_mg";
+		};
+		class Unit3 {
+		position[] = {6,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_mg_asst";
+		};
+		class Unit4 {
+		position[] = {3,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_mg_asst";
+		};
+		class Unit5 {
+		position[] = {4,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_launcher";
+		};
+		class Unit6 {
+		position[] = {5,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_launcher_asst";
+		};
+		class Unit7 {
+		position[] = {6,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+		class Unit8 {
+		position[] = {3,-5,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+	};
+
+	class ncraf_infantry_at_team {
+		name = "Anti-Tank Team";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+		raritygroup = 0.5;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "CORPORAL";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_tl";
+		};
+		class Unit1 {
+		position[] = {3,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_launcher";
+		};
+		class Unit2 {
+		position[] = {4,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_launcher_asst";
+		};
+	};
+
+	class ncraf_infantry_command_team {
+		name = "Command Team";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+		raritygroup = 0.3;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "LIEUTENANT";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_junior_officer";
+		};
+		class Unit1 {
+		position[] = {3,-3,0};
+		rank = "SERGEANT";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_sl";
+		};
+		class Unit2 {
+		position[] = {4,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_medic";
+		};
+		class Unit3 {
+		position[] = {5,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+	};
+		};
+
+	// pi if i have you bugtest this im so sorry
+	// hours of just writing shitty fucking code. half of this doesnt even make sense, but im 99% it'll work.
+	
+	class SpecOps {
+		name = "Special Forces";
+	
+	class ncraf_ranger_patrol {
+		name = "Ranger Patrol";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+		raritygroup = 0.2;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_patrol_ranger_repeater";
+		};
+		class Unit1 {
+		position[] = {3,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_patrol_ranger_rifle";
+		};
+	};
+	
+	class ncraf_ranger_team {
+		name = "Ranger Patrol";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+		raritygroup = 0.15;
+	
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_patrol_ranger_repeater";
+		};
+		class Unit1 {
+		position[] = {3,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_patrol_ranger_rifle";
+		};
+		class Unit2 {
+		position[] = {4,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_patrol_ranger_rifle";
+		};
+		class Unit3 {
+		position[] = {5,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_patrol_ranger_automatic";
+		};
+	};
+
+	class ncraf_vet_ranger_team {
+		name = "Veteran Ranger Team";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+		raritygroup = 0.07;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_vet_ranger_repeater";
+		};
+		class Unit1 {
+		position[] = {3,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_vet_ranger_repeater";
+		};
+	};
+
+	class ncraf_sniper_team {
+		name = "Sniper Team";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+		raritygroup = 0.15;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_recon_sniper";
+		};
+		class Unit1 {
+		position[] = {3,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_recon_spotter";
+		};
+	};
+
+	class ncraf_scout_team {
+		name = "Scout Team";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+		raritygroup = 0.25;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "SERGEANT";
+		side = 1;
+		vehicle = "ncraf_ncr_recon_leader";
+		};
+		class Unit1 {
+		position[] = {3,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_recon_scout";
+		};
+		class Unit2 {
+		position[] = {4,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_recon_scout";
+		};
+		class Unit3 {
+		position[] = {5,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_recon_scout";
+		};
+	};
+	};
+	
+	class Motorized {
+		class ncraf_motor_motsquad {
+		name = "Motor Squad";
+		side = 1;
+		faction = "ncraf_ncr";
+		icon = "\A3\ui_f\data\map\markers\nato\o_motor_inf.paa";
+		raritygroup = 0.6;
+		
+		class Unit0 {
+		position[] = {0,0,0};
+		rank = "SERGEANT";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_sl";
+		};
+		class Unit1 {
+		position[] = {4,-1,0};
+		rank = "CORPORAL";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_tl";
+		};
+		class Unit2 {
+		position[] = {5,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_mg";
+		};
+		class Unit3 {
+		position[] = {6,-1,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_mg_asst";
+		};
+		class Unit4 {
+		position[] = {3,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_mg_asst";
+		};
+		class Unit5 {
+		position[] = {4,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_launcher";
+		};
+		class Unit6 {
+		position[] = {5,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper_launcher_asst";
+		};
+		class Unit7 {
+		position[] = {6,-3,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+		class Unit8 {
+		position[] = {3,-5,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_trooper";
+		};
+		class Unit9 {
+		position[] = {10,0,0};
+		rank = "PRIVATE";
+		side = 1;
+		vehicle = "ncraf_ncr_utilitytruck";
+		};
+		};
+	};
+	
+	
+	class Support {
+		name = "Support Infantry";
+	};
+		
+	class Mechanized {
+		name = "Mechanized Infantry";
+
+		class ncraf_ncr_heavy_squad {
+			name = "Heavy Trooper Squad";
+			side = 1;
+			faction = "ncraf_ncr";
+			icon = "\A3\ui_f\data\map\markers\nato\o_mech_inf.paa";
+			rarityGroup= 0.3;
+			
+			class Unit0 {
+			positon[] = {0,0,0};
+			rank = "SERGEANT";
+			side = 1;
+			vehicle = "ncraf_ncr_heavy_trooper_sl";
+			};
+			class Unit1 {
+			positon[] = {3,-1,0};
+			rank = "PRIVATE";
+			side = 1;
+			vehicle = "ncraf_ncr_heavy_trooper";
+			};
+			class Unit2 {
+			positon[] = {4,-1,0};
+			rank = "PRIVATE";
+			side = 1;
+			vehicle = "ncraf_ncr_heavy_trooper";
+			};
+			class Unit3 {
+			positon[] = {5,-1,0};
+			rank = "PRIVATE";
+			side = 1;
+			vehicle = "ncraf_ncr_heavy_trooper";
+			};
+			class Unit4 {
+			positon[] = {6,-1,0};
+			rank = "PRIVATE";
+			side = 1;
+			vehicle = "ncraf_ncr_heavy_trooper_mg";
+			};
+		};
+        class Armored {
+            name = "Armor";
+
+        };
+
+        class Artillery {
+            name = "Artillery";
+
+        };
+
+        class Naval {
+             name = "Naval";
+
+        };
+
+        class Air {
+            name = "Air";
+
+		};
+	};
+};
+};
